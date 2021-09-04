62214247d267c8e97b342783fb00d461957f5c1a
sultan_alrefaei <d.f.s.programmer@gmail.com>
2021-09-04 21:24:18 +0300

    Add Space To Code

--- SmoothFollow.cs
+++ SmoothFollow.cs
@@ -21,7 +21,6 @@ public class SmoothFollow : MonoBehaviour
     // The target we are following
     //[Header("Target object")]
     Transform target;
-    
     // The distance in the x-z plane to the target
     [Header("The distance between camera and object")]
     [Range(0, 50)]
